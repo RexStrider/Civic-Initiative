c2c4ac2c28834195228636a9092a513cd34674ca
Odysseus <arwaterman@gmail.com>
2019-09-07 13:34:30 -0700

    creating basic server script with express js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// serve up the optimized build files if we are in the production environment
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 8010;
+app.listen(PORT, () => console.log(`running server on PORT ${PORT}`));
