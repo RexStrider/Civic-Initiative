ce09d98587005bb529881f91c2d8437ab5442588
Odysseus <arwaterman@gmail.com>
2019-09-08 13:02:34 -0700

    renamed civic info key

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,5 @@ require('dotenv');
 
 module.exports = {
     news_api_key: process.env.NEWS_API_KEY,
-    google_civic_information: process.env.GOOGLE_CIVIC_INFORMATION_KEY
+    google_civic_information_key: process.env.GOOGLE_CIVIC_INFORMATION_KEY
 };
