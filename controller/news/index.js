106b204a2addd4d0e12332d54c9fc1a46c0b30c3
Odysseus <arwaterman@gmail.com>
2019-09-08 13:04:01 -0700

    added controller for news api

---
+++ controller/news/index.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const keys = require('../../config/keys');
+
+exports.getNews = async (req, res) => {
+    try {
+        const pageNum = req.params.pageNum || 1;
+        const query = req.params.query || '';
+
+        if (query === '') {
+            res.json({ message: 'query was not provided', body: null });
+        }
+
+        const URL = `https://newsapi.org/v2/everything?` +
+                    `q=(${query})&` +
+                    `page=${pageNum}&` +
+                    `apiKey=${keys.news_api_key}`;
+        const response = await axios.get(URL);
+
+        res.json({ message: "successfully called news api", body: response.data });
+    }
+    catch (error) {
+        res.json({ message: error, body: null });
+    }
+}
