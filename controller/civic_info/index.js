1b33571dfd966d3df331936e2bae9eb9c611c49f
Odysseus <arwaterman@gmail.com>
2019-09-08 13:04:54 -0700

    added controller for civic info

---
+++ controller/civic_info/index.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+const keys = require('../../config/keys');
+
+exports.getCivicInfo = async (req, res) => {
+    try {
+        const address = req.params.address || ''
+
+        if (address === '') {
+            res.json({ message: 'no address given', body: null });
+        }
+
+        const URL = `https://www.googleapis.com/civicinfo/v2/representatives?key=${keys.google_civic_information_key}&address=${address}`;
+        const response = await axios.get(URL);
+
+        res.json({ message: "civic information api was successfully called", body: response.data });
+    }
+    catch(error) {
+        res.json({ message: error, body: null });
+    }
+}
