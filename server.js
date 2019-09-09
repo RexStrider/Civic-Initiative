478fd76b51b76f26b27d5f1adb1e224f2d7e10d6
Odysseus <arwaterman@gmail.com>
2019-09-09 12:31:40 -0700

    added routing to api

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api', routes);
 
 // serve up the optimized build files if we are in the production environment
 if (process.env.NODE_ENV === 'production') {
