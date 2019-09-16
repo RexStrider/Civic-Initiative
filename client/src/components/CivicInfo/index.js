ccd99a41d6272f973304edef147f7072c293507a
Odysseus <arwaterman@gmail.com>
2019-09-16 11:55:46 -0700

    added submit button for civic information component, connected api call to submit button

--- client/src/components/CivicInfo/index.js
+++ client/src/components/CivicInfo/index.js
@@ -10,11 +10,16 @@ class CivicInfo extends Component {
             <section className='civic-info'>
                 <label>Search for a representative </label>
                 <input
-                    name='address'
-                    value={this.props.address}
-                    onChange={this.props.handleInput}
-                    onKeyPress={this.props.handleKeyPress}
-                    placeholder='enter an address' />
+                name='address'
+                value={this.props.address}
+                onChange={this.props.handleInput}
+                onKeyPress={this.props.handleCivicApiCall}
+                placeholder='enter an address' />
+                <button
+                type="submit"
+                onClick={this.props.handleCivicApiCall} >
+                    Submit
+                </button>
             </section>
         );
     }
