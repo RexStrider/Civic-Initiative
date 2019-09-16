6467739f876015edd765c96bbba41d1e06db1182
Odysseus <arwaterman@gmail.com>
2019-09-16 12:02:34 -0700

    added civic card component

---
+++ client/src/components/CivicCard/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CivicCard extends Component {
+    render() { 
+        return (
+            <section className='civic-card'>
+                Representative info goes here...
+            </section>
+        );
+    }
+}
+ 
+export default CivicCard;
