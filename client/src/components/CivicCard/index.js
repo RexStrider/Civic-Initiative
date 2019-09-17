cd869c2bb16dba6056622c9c030991805e1a48d8
Odysseus <arwaterman@gmail.com>
2019-09-17 16:50:31 -0700

    civic cards are now rendering a represetnatives information

--- client/src/components/CivicCard/index.js
+++ client/src/components/CivicCard/index.js
@@ -1,73 +1,111 @@
+import './CivicCard.css';
+
 import React, { Component } from 'react';
 
-import pumpkinhead_photo from '../../assets/images/pumpkinhead.jpg';
-
 class CivicCard extends Component {
-    representative = {
-        name: "pumpkin head",
-        title: "president?",
-        government: "United States",
-        addresses: [["123 white house", "Washington DC"]],
-        phones: ["111-222-3333"],
-        facebookId: "pumpkinhead",
-        twitterId: null,
-        otherId: "",
-        photoUrl: pumpkinhead_photo,
-        urls: ["https://www.whitehouse.gov"]
-    }
     render() {
-        let {start, end} = 0;
-        // determines how much of the url to a representatives website is displayed on the card
-        // while maintaining the full url for the anchor tag
-        if (this.representative.urls) {
-            const url = this.representative.urls[0]
-
-            // determines if the url contains https or http
-            start = url.substring(0, 5) === 'https' ? 8 : 7;
-            // determines if the url has a '/' character at the end
-            end = url.substring(url.length-1, url.length) === '/' ? url.length-1 : url.length;
-        }
+        const links = this.getLinks(this.props.representative);
         return (
-            <section className='civic-card'
-                     key={ this.representative.name }>
+            <section className='civic-card'>
                 <section className='card-body'>
-                    { this.representative.photoUrl ? <img src={this.representative.photoUrl} alt='' style={{width: '200px'}}/> : null }
-                    <h5 className='card-title'>{ this.representative.name }</h5>
-                    <h6 className='card-subtitle'>{ this.representative.title }</h6>
-                    <h6 className='card-subtitle'>{ this.representative.government }</h6>
-                    { this.renderPhones(this.representative.phones) }
-                    { this.renderAddresses(this.representative.addresses) }
-                    <section className='channels'>
-                    
-                        { this.representative.facebookId
-                        ? (<a href={(`https://www.facebook.com/${ this.representative.facebookId }`)}
-                              className="card-link">Facebook</a>)
-                        : null }
-                        
-                        { this.representative.twitterId
-                        ? (<a href={(`https://twitter.com/${ this.representative.twitterId }`)}
-                              className="card-link">Twitter</a>)
-                        : null }
+                    <section className='card-info'>
+                        { this.props.representative.photoUrl ? <img src={this.props.representative.photoUrl} alt='' style={{width: '200px'}}/> : null }
 
-                        { this.representative.otherId
-                        ? (<a href={(`#`)}
-                              className="card-link">this.representative.otherId</a>)
-                        : null }
+                        <section>
+                            <h3 className='card-title'>{ this.props.representative.name }</h3>
+                            <h4 className='card-subtitle'>{ this.props.representative.title }</h4>
+                            <h4 className='card-subtitle'>{ this.props.representative.government }</h4>
+                        </section>
                     </section>
 
-                    { this.representative.urls
-                        ? ( <a href={this.representative.urls[0]}
-                               className="card-link">{ this.representative.urls[0].substring(start, end) }
-                            </a> )
-                        : null }
+                    <section className='card-contact'>
+                        { this.renderAddresses(this.props.representative.addresses) }
+                        { this.renderPhones(this.props.representative.phones) }
+                    </section>
+
+                    <section className='card-links'>
+                        { this.renderCardLinks(links) }
+                    </section>
                 </section>
             </section>
         );
     }
 
+    getLinks = representative => {
+        const links = [];
+
+        if (representative.urls) {
+            links.push(
+                <a href={representative.urls[0]} >
+                    Official Website
+                </a>
+            );
+        }
+
+        // if (representative.otherId) {
+        //     links.push(
+        //         <a href={representative.otherId} >
+        //             { representative.otherId }
+        //         </a>
+        //     );
+        // }
+
+        if (representative.facebookId) {
+            links.push(
+                <a href={(`https://www.facebook.com/${ representative.facebookId }`)}
+                className="card-link">
+                    Facebook
+                </a>
+            );
+        }
+
+        if (representative.twitterId) {
+            links.push(
+                <a href={(`https://twitter.com/${ representative.twitterId }`)}
+                className="card-link">
+                    Twitter
+                </a>
+            );
+        }
+
+        if (representative.youtubeId) {
+            let href = '';
+            if (representative.youtubeId.length > 23) href = `https://www.youtube.com/channel/${representative.youtubeId}`;
+            else href = `https://www.youtube.com/user/${representative.youtubeId}`;
+            links.push(
+                <a href={( href )}
+                className="card-link">
+                    YouTube
+                </a>
+            );
+        }
+
+        return links;
+    }
+
+    renderCardLinks = links => {
+        const result = [];
+        if (links)
+        for (let i=0; i < links.length; i++) {
+            if (i % 2 === 1) result.push(this.renderLinkRow(links[i-1], links[i], `key_${i}`));
+        }
+        if ((links.length-1) % 2 === 0) result.push(this.renderLinkRow(links[links.length-1], null, `key_${links.length-1}`));
+        return result;
+    }
+
+    renderLinkRow = (linkA, linkB, key) => {
+        return(
+            <section key={key} className='card-link-row'>
+                {linkA}
+                {linkB}
+            </section>
+        )
+    }
+
     // creates the phone number components and returns them in an array
     renderPhones(phoneNumbers) {
         const ary = [];
+        if (phoneNumbers)
         for (let i=0; i<phoneNumbers.length; i++) {
             const phone = phoneNumbers[i];
             ary.push(this.renderPhoneNumber(phone));
@@ -78,6 +116,7 @@ class CivicCard extends Component {
     // creates the address components and returns them in an array
     renderAddresses(addresses) {
         const ary = []
+        if (addresses)
         for (let i=0; i<addresses.length; i++) {
             const address = addresses[i];
             ary.push(this.renderAddress(address));
