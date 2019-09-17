76caa025d03528baae55bc2d494b94a6293809b0
Odysseus <arwaterman@gmail.com>
2019-09-17 16:51:13 -0700

    civic information component now renders all civic cards of each representative retrieved from the google civic information api

--- client/src/components/CivicInfo/index.js
+++ client/src/components/CivicInfo/index.js
@@ -1,30 +1,55 @@
 import './CivicInfo.css';
 
 import React, { Component } from 'react';
+import SearchBar from '../SearchBar';
 import CivicCard from '../CivicCard';
 
+import pumpkinhead_photo from '../../assets/images/pumpkinhead.jpg';
+
 class CivicInfo extends Component {
+
+    representative = {
+        name: "pumpkin head",
+        title: "president?",
+        government: "United States",
+        addresses: [["123 white house", "Washington DC"]],
+        phones: ["111-222-3333"],
+        facebookId: "pumpkinhead",
+        twitterId: null,
+        otherId: "",
+        photoUrl: pumpkinhead_photo,
+        urls: ["https://www.whitehouse.gov"]
+    }
+
     render() {
         return (
-            
             <section className='civic-info'>
-                <section className='civic-search'>
-                    <label>Search for a representative </label>
-                    <input
-                    name='address'
-                    value={this.props.address}
-                    onChange={this.props.handleInput}
-                    onKeyPress={this.props.handleCivicApiCall}
-                    placeholder='enter an address' />
-                    <button
-                    type="submit"
-                    onClick={this.props.handleCivicApiCall} >
-                        Submit
-                    </button>
-                </section>
+                <SearchBar
+                address={this.props.address}
+                handleInput={this.props.handleInput}
+                handleCivicApiCall={this.props.handleCivicApiCall}
+                {...this.props} />
 
-                <CivicCard />
+                { this.props.civicData
+                    ? this.renderCivicData(this.props.civicData)
+                    : null }
             </section>
+        );
+    }
+
+    renderCivicData = civicData => {
+        const result = [];
+
+        for (let i=0; i < civicData.length; i++) {
+            result.push(this.renderCivicCard(civicData[i]));
+        }
+
+        return result;
+    }
+
+    renderCivicCard = representative => {
+        return (
+            <CivicCard representative={ representative } key={ representative.name } />
         );
     }
 }
