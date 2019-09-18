589cdc5b55ce474e45b2183a24c3371071e0471f
Odysseus <arwaterman@gmail.com>
2019-09-17 22:55:21 -0700

    updated search bar styling, mobile friendly, larger font, ect

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -7,17 +7,19 @@ class SearchBar extends Component {
         return (
             <section className='search-bar'>
                 <label>Search for a representative </label>
-                <input
-                name='address'
-                value={this.props.address}
-                onChange={this.props.handleInput}
-                onKeyPress={this.props.handleCivicApiCall}
-                placeholder='enter an address' />
-                <button
-                type="submit"
-                onClick={this.props.handleCivicApiCall} >
-                    Submit
-                </button>
+                <section>
+                    <input
+                    name='address'
+                    value={this.props.address}
+                    onChange={this.props.handleInput}
+                    onKeyPress={this.props.handleCivicApiCall}
+                    placeholder='enter an address' />
+                    <button
+                    type="submit"
+                    onClick={this.props.handleCivicApiCall} >
+                        Submit
+                    </button>
+                </section>
             </section>
         );
     }
