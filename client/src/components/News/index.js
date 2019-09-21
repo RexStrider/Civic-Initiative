6621389011b8e7c360bbaf91b808a564b3e59fb7
Odysseus <arwaterman@gmail.com>
2019-09-21 12:07:17 -0700

    added styling to News component, added temporary error message to news component

--- client/src/components/News/index.js
+++ client/src/components/News/index.js
@@ -1,13 +1,21 @@
+import './News.css';
+
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class News extends Component {
     render() {
         console.log(this.props);
+
+        console.log(this.props.articles);
+
         return (
             <section className='News'>
-                {this.props.articles.map((article, key) => {
+                { this.props.articles && this.props.articles.length > 0
+                ? this.props.articles.map((article, key) => {
                     return this.renderNewsCard(article, key);
-                })}
+                })
+                : this.renderError() }
             </section>
         );
     }
@@ -25,6 +33,18 @@ class News extends Component {
                     <a href={article.url}>{ article.source.name}</a>
 
                 </section>
+            </section>
+        );
+    }
+
+    renderError = () => {
+        return (
+            <section className='news-card'>
+                <h1>
+                    Unfortunately we could not find any articles that mentioned your representative.
+                </h1>
+                <p>This issue might have occurred because the news page was refreshed. We understand this is frustrating and we are currently working on this issue. In the meantime, I recommend going back to the main page.</p>
+                <Link to='/'>Main page</Link>
             </section>
         );
     }
